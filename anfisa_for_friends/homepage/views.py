563e4522333134ee3a4981dc4f38b403d3504e55
Evgeny Belyakov <belyash84.belyakov@yandex.ru>
2025-04-23 23:45:23 +0300

    Set filter, order and limit for homepage data

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
@@ -5,7 +6,9 @@ from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('title', 'id', 'description')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(Q(is_published=True) & Q(is_on_main=True)).order_by('title')[:3]
     context = {
         'ice_cream_list': ice_cream_list,
     }
