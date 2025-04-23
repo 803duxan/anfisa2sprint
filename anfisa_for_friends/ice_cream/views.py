b074e313dc350dcd16782e9dea670abb1594ede2
Evgeny Belyakov <belyash84.belyakov@yandex.ru>
2025-04-24 00:36:42 +0300

    Change get_objects_or_404 function arguments in ice_cream/views.py in ice_cream_detail function

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -5,7 +5,12 @@ from ice_cream.models import IceCream
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
-    ice_cream = get_object_or_404(IceCream, pk=pk)
+    ice_cream = get_object_or_404(
+        IceCream.objects.values(
+            'title', 'description'
+        ).filter(is_published=True),
+        pk=pk
+    )
     context = {
         'ice_cream': ice_cream,
     }
